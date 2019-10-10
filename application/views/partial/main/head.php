296e56093f966611debee175f7e800710c235927
Yusril Fahmi Al Faizi <44188722+yusrilfahmialfaizi@users.noreply.github.com>
2019-10-10 21:30:36 +0700

    asd

--- application/views/partial/main/head.php
+++ application/views/partial/main/head.php
@@ -11,20 +11,20 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
 
     <!-- Bootstrap Core Css -->
-    <link href="<?php base_url("assets/plugins/bootstrap/css/bootstrap.css") ?>" rel="stylesheet">
+    <link href="<?php echo base_url("assets/plugins/bootstrap/css/bootstrap.css") ?>" rel="stylesheet">
 
     <!-- Waves Effect Css -->
-    <link href="<?php base_url("assets/plugins/node-waves/waves.css") ?>" rel="stylesheet" />
+    <link href="<?php echo base_url("assets/plugins/node-waves/waves.css") ?>" rel="stylesheet" />
 
     <!-- Animation Css -->
-    <link href="<?php base_url("assets/plugins/animate-css/animate.css") ?>" rel="stylesheet" />
+    <link href="<?php echo base_url("assets/plugins/animate-css/animate.css") ?>" rel="stylesheet" />
 
     <!-- Morris Chart Css-->
-    <link href="<?php base_url("assets/plugins/morrisjs/morris.css") ?>" rel="stylesheet" />
+    <link href="<?php echo base_url("assets/plugins/morrisjs/morris.css") ?>" rel="stylesheet" />
 
     <!-- Custom Css -->
-    <link href="<?php base_url("assets/css/style.css") ?>" rel="stylesheet">
+    <link href="<?php echo base_url("assets/css/style.css") ?>" rel="stylesheet">
 
     <!-- AdminBSB Themes. You can choose a theme from css/themes instead of get all themes -->
-    <link href="<?php base_url("assets/css/themes/all-themes.css") ?>" rel="stylesheet" />
+    <link href="<?php echo base_url("assets/css/themes/all-themes.css") ?>" rel="stylesheet" />
 </head>
